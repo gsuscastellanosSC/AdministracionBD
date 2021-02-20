52301a19c1456c51b47f3b22e4aabc4a262ef6ca
SC <jesuscastellanospaez@gmail.com>
2021-02-20 13:12:21 -0500

    Class#73
    Signed-off-by: SC <jesuscastellanospaez@gmail.com>

--- Recursos/recursos (2).sql
+++ Recursos/recursos (2).sql
@@ -19,6 +19,5 @@ ALTER TABLESPACE TBS3
       NEXT 512K
       MAXSIZE 250M;
       
-      
-ALTER DATABASE DATAFILE '/home/oracle/bbdd/stuff01.dbf'
+ ALTER DATABASE DATAFILE '/home/oracle/bbdd/stuff01.dbf'
    RESIZE 100M;
